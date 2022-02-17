cb4c6e0b24f0b5abd2a8d9e8129d8d5f40424e82
xrustuna.di@gmail.com <99490012+Khrystyna77@users.noreply.github.com>
2022-02-17 21:42:35 +0200

    test

--- solution.go
+++ solution.go
@@ -7,7 +7,7 @@ package square
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
-//go
+//go ahead
 
 func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
 }
