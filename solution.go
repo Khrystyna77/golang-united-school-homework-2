91c0951f538babe7979a31f1fa071069a81fb58b
Khrystyna77 <xrustuna.di@gmail.com>
2022-02-18 14:50:04 +0200

    ow

--- solution.go
+++ solution.go
@@ -7,7 +7,7 @@ package square
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
-//go ahead 22
+//go ahead 223
 
 func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
 }
