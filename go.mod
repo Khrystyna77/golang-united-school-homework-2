b879bdd8541d08b10e66648561008c3d34fbf69f
Khrystyna77 <xrustuna.di@gmail.com>
2022-02-20 22:40:32 +0200

    solution

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goahead-1
+
+go 1.17
--- solution.go
+++ solution.go
@@ -1,4 +1,8 @@
-package square
+package solution
+
+import (
+	"math"
+)
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
@@ -7,7 +11,29 @@ package square
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
-//go ahead MFA2
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type myint int
+
+const SidesTriangle myint = 3
+const SidesSquare myint = 4
+const SidesCircle myint = 0
+
+func CalcSquare(sideLen float64, sidesNum myint) float64 {
+	var Pi64 float64 = math.Pi
+	var S float64
+
+	if sidesNum == SidesTriangle {
+		S = sideLen * 2
+
+	} else if sidesNum == SidesSquare {
+
+		S = sideLen * sideLen
+
+	} else if sidesNum == SidesCircle {
+
+		S = Pi64 * (sideLen * sideLen)
+
+	}
+
+	return S
 }
